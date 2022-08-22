4435a462e77dedbd7c564c2df34c99334a0e3e70
andreas-queenslab <andreas@queenslab.se>
2022-08-22 15:34:05 +0200

    netlify config instead

---
+++ src/entry.netlify.ts
@@ -0,0 +1,7 @@
+import { qwikCity } from "@builder.io/qwik-city/middleware/netlify-edge";
+import render from "./entry.ssr";
+
+const qwikCityHandler = qwikCity(render);
+
+export default qwikCityHandler;
+
